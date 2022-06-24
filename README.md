f687eb4d78070e42e570e72eb1f64dc07d533a16
CAI-PC\Cai <chencaihappy@hotmail.com>
2022-06-24 15:26:25 +0800

    [dev]XUAA demos

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# SAPBTP_XUAA_SIMPLE
-user XUAA to security our Apps
+### 目录
+- [目录](#目录)
+  - [摘要](#摘要)
+  - [部署](#部署)
+  - [参考](#参考)
+
+#### 摘要
+该demo的主要目标是建立两个应用程序， approuter 和 productlist ， 用户通过访问approouter 来实现对productlist applicaiton 进行安全认证。认证方式采用oauth + SAP IDP , 用户可通过登录SAPBTP的用户方式来登录并访问productlist applciation
+
+#### 部署
+
+部署采用cloud foundry命令行来实现, 开发人员进入到product-list文件夹，并使用以下命令行部署
+
+```
+cf api <api endpoint>
+cf login
+cf push
+
+```
+
+#### 参考
+
+* [SAP Authorization and Trust Management 服务 教程](https://help.sap.com/docs/BTP/65de2977205c403bbc107264b8eccf4b/902ae800c1d04c7388e407b7815e5cc8.html)
+* [cp-cf-security-xsuaa](https://developers.sap.com/tutorials/cp-cf-security-xsuaa-create.html)
+* [SAP XUAA Pilot App](https://github.com/SAP-samples/teched2019-cloud-cf-product-list/tree/sap-tutorial-xsuaa)
---
+++ product-list/myapp/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { getProducts, getProductsByName} = require('./lib/repository');
+const app = express();
+const port = process.env.port || 8080;
+// secure the direct call to the application
+
+const passport = require('passport');
+const { JWTStrategy } = require('@sap/xssec');
+const xsenv = require('@sap/xsenv');
+
+// XSUAA Middleware
+passport.use(new JWTStrategy(xsenv.getServices({uaa:{tag:'xsuaa'}}).uaa));
+
+app.use(passport.initialize());
+app.use(passport.authenticate('JWT', { session: false }));
+
+
+app.get('/products', checkReadScope, getProducts);
+
+// Scope check
+function checkReadScope(req, res, next) {
+	if (req.authInfo.checkLocalScope('read')) {
+		return next();
+	} else {
+    	console.log('Missing the expected scope');
+    	res.status(403).end('Forbidden');
+	}
+}
+
+
+// Serve static files
+app.use('/', express.static('static/'));
+
+app.listen(port, () => {
+	console.log('%s listening at %s', app.name, port);
+})
